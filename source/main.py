c7144b13c1902b35996f0d1a8b70f2e0611a54b2
Vipqix <101670879+Vipqix@users.noreply.github.com>
2024-10-30 17:44:57 +0100

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,3 +1,7 @@
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: main.py
+# Bytecode version: 3.11a7e (3495)
+# Source timestamp: 1970-01-01 00:00:00 UTC (0)
 
 import hid
 import os
